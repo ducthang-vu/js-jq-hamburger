044654cc6cbf34da2844f4ebee0d96b12f8182cf
Duc Thang Vu <vuducthang91@gmail.com>
2020-04-08 15:09:43 +0200

    global variables declared

---
+++ main.js
@@ -0,0 +1,12 @@
+/* A script handling the hamburger menu element */
+
+console.log('main.js is working')
+console.log($)
+
+
+/* GLOBAL VARIABLES */
+const openButton = $('.header-right > a')
+const closeButton = $('.hamburger-menu a.close')
+const menu = $('header .hamburger-menu')
+
+
